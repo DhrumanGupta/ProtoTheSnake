1027fab0fdce3b4c8fcbeb3fcb5f606e0c96cb16
Berlm <berlm.business@gmail.com>
2021-04-02 02:02:37 +0530

    Added config to gitignore

---
+++ DiscordSnakeBot/Services/StartupService.cs
@@ -0,0 +1,28 @@
+﻿using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace DiscordSnakeBot.Services
+{
+    public class StartupService
+    {
+        public static IServiceProvider Provider;
+        private readonly DiscordSocketClient _discord;
+        private readonly CommandService _commands;
+        private readonly IConfigurationRoot _config;
+
+        public StartupService(IServiceProvider provider, DiscordSocketClient discord, CommandService commands, IConfigurationRoot config)
+        {
+            Provider = provider;
+            _discord = discord;
+            _commands = commands;
+            _config = config;
+        }
+
+        public async Task StartAsync()
+        {
+            string token = _config["tokens:discord"];
+        }
+    }
+}
